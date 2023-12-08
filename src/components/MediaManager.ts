03c1dd60a19a873c39f4b4b8a5e802d991006ab6
radsylph <andresa@devtop.io>
2023-12-08 01:28:27 -0400

    feat:added new methods to review movie

--- src/components/MediaManager.ts
+++ src/components/MediaManager.ts
@@ -15,6 +15,7 @@ import {
   MovieInterface,
   SerieInterface,
   MediaInterface,
+  ReviewInterface,
 } from "../interfaces/main";
 
 interface CustomRequest extends Request {
@@ -102,8 +103,6 @@ class MediaManager {
     try {
       const movie = await Movie.findOne({ idApi: id });
       if (!movie) {
-        const endpoint = endpoints.getMovieDetails + id;
-        console.log(endpoint);
         const movieDetails = await AII.get(endpoints.getMovieDetails + id).then(
           async (movieDetailed) => {
             const movie_trailers = await AII.get(
@@ -139,6 +138,7 @@ class MediaManager {
             };
             try {
               await Movie.create(movie);
+              return movie;
             } catch (error) {
               console.log(error);
             }
@@ -162,6 +162,133 @@ class MediaManager {
             msg: "there was an error when finding the movie",
             path: "MovieManager",
             location: "getMovie",
+          },
+        ],
+      });
+    }
+  }
+
+  async getSerie(req: CustomRequest, res: Response) {
+    const { id } = req.params;
+    try {
+      const serie = await Serie.findOne({ idApi: id });
+      if (!serie) {
+        const serieDetails = await AII.get(endpoints.getSerieDetails + id).then(
+          async (serieDetailed) => {
+            const serie_trailers = await AII.get(
+              endpoints.getSerieDetails + id + endpoints.getSerieVideo
+            ).then((trailers) => {
+              const serieTrailers = trailers.data.results.map(
+                (trailer: any) => endpoints.showVideo + trailer.key
+              );
+              return serieTrailers;
+            });
+            const serie_genres = await Genres.find({
+              id: {
+                $in: serieDetailed.data.genres.map((genre: any) => genre.id),
+              },
+            });
+            const serie: SerieInterface = {
+              idApi: serieDetailed.data.id,
+              name: serieDetailed.data.name,
+              overview: serieDetailed.data.overview,
+              tagline: serieDetailed.data.tagline,
+              posters: [
+                endpoints.showImages + serieDetailed.data.poster_path,
+                endpoints.showImages + serieDetailed.data.backdrop_path,
+              ],
+              firstAir: serieDetailed.data.first_air_date,
+              lastAir: serieDetailed.data.last_air_date,
+              totalEpisodes: serieDetailed.data.number_of_episodes,
+              totalSeasons: serieDetailed.data.number_of_seasons,
+              genres: serie_genres.map((genre) => genre.name),
+              trailers: serie_trailers ? serie_trailers : "",
+              status: serieDetailed.data.status,
+            };
+            console.log(serie);
+            try {
+              await Serie.create(serie);
+              return serie;
+            } catch (error) {
+              console.log(error);
+              return error;
+            }
+          }
+        );
+        return res
+          .status(200)
+          .json({ msg: "Serie from api", serie: serieDetails });
+      }
+      return res.status(200).json({ msg: "Serie from db", serie });
+    } catch (error) {
+      return res.status(500).json({
+        message: "server error",
+        errors: [
+          {
+            type: "server",
+            value: error,
+            msg: "there was an error when finding the serie",
+            path: "MovieManager",
+            location: "getSerie",
+          },
+        ],
+      });
+    }
+  }
+
+  async reviewMovie(req: CustomRequest, res: Response) {
+    const { id } = req.params;
+    const { review, rating } = req.body;
+
+    const serie = await Movie.findOne({ idApi: id });
+    if (!serie) {
+      return res.status(404).json({ msg: "Movie not found" });
+    }
+
+    try {
+      const newReview = await Review.create({
+        review,
+        rating,
+        mediaId: serie._id,
+        owner: req.user_id,
+      });
+
+      await Movie.updateOne(
+        { idApi: id },
+        {
+          $inc: { voteCount: 1, voteTotalPoints: rating },
+        }
+      );
+
+      const updatedMovie = await Movie.findOne({ idApi: id });
+      if (
+        !updatedMovie ||
+        updatedMovie.voteTotalPoints === undefined ||
+        updatedMovie.voteCount === undefined
+      ) {
+        return res
+          .status(404)
+          .json({
+            msg: "Updated movie not found or voteTotalPoints/voteCount is undefined",
+          });
+      }
+      updatedMovie.voteAverage =
+        updatedMovie.voteTotalPoints / updatedMovie.voteCount;
+      await updatedMovie.save();
+
+      await newReview.save();
+
+      return res.status(200).json({ msg: "Review created", review: newReview });
+    } catch (error) {
+      return res.status(500).json({
+        message: "server error",
+        errors: [
+          {
+            type: "server",
+            value: error,
+            msg: "there was an error when finding the serie",
+            path: "MovieManager",
+            location: "reviewMovie",
           },
         ],
       });
