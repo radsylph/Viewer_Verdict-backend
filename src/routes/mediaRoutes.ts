b5006bea186f7f5c5fcc2d455971dd451c2f156d
radsylph <andresa@devtop.io>
2023-12-13 02:08:56 -0400

    feat: added more routes for reviewing

--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -6,15 +6,30 @@ import {
   getMovie,
   getSerie,
   reviewMovie,
+  editReviewMovie,
+  deleteReviewMovie,
+  reviewSerie,
+  editReviewSerie,
+  deleteReviewSerie,
+  CommentReview,
 } from "../controllers/mediaControllers";
 import getUserInfo from "../middlewares/ProtectRutes";
 
 const router = express.Router();
 
 router.route("/general/:name").get(searchMedia);
+
 router.route("/movie/:id").get(getMovie);
-router.route("/serie/:id").get(getSerie);
 router.route("/movie/:id/review").post(getUserInfo, reviewMovie);
+router.route("/movie/:id/review").put(getUserInfo, editReviewMovie);
+router.route("/movie/:id/review").delete(getUserInfo, deleteReviewMovie);
+
+router.route("/serie/:id").get(getSerie);
+router.route("/serie/:id/review").post(getUserInfo, reviewSerie);
+router.route("/serie/:id/review").put(getUserInfo, editReviewSerie);
+router.route("/serie/:id/review").delete(getUserInfo, deleteReviewSerie);
+
+router.route("/review/:id").post(getUserInfo, CommentReview);
 
 router.route("/admin/uploadGenres").post(uploadGenres);
 router.route("/admin/deleteGenres").delete(deleteGenres);
