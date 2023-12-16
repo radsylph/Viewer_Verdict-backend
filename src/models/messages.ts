515b08eb1b8409fd70d2ad4cbf612db15713ec84
radsylph <andresa@devtop.io>
2023-12-16 02:17:12 -0400

    feat: edited messages model

--- src/models/messages.ts
+++ src/models/messages.ts
@@ -1,20 +1,37 @@
 import mongoose from "mongoose";
 import { MessageInterface } from "../interfaces/main";
 
-const messageSchema = new mongoose.Schema<MessageInterface>({
-  message: {
-    type: String,
-    required: true,
+const messageSchema = new mongoose.Schema<MessageInterface>(
+  {
+    idRoom: {
+      type: String,
+      required: true,
+    },
+    idSender: {
+      type: String,
+      required: true,
+    },
+    idSentTo: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    sendTo: {
+      type: String,
+      required: true,
+    },
+    sender: {
+      type: String,
+      required: true,
+    },
   },
-  sender: {
-    type: String,
-    required: true,
-  },
-  idSender: {
-    type: String,
-    required: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Message = mongoose.model<MessageInterface>("Message", messageSchema);
 export default Message;
